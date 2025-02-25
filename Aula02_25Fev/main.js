35464572a1cd7b699882ccd694f0c8405ad74cd6
mmamorim <marc.m.amorim@gmail.com>
2025-02-25 20:44:41 -0300

    v1

---
+++ Aula02_25Fev/main.js
@@ -0,0 +1,17 @@
+
+// LAÇOS DE REPETIÇÃO FOR
+//for(let x=5; x<10; x++) {
+//    console.log("oi");    
+//}
+
+// STRINGS
+let nome = "Maria"
+let idade = 25
+let texto1 = "oi '"+nome+"' você tem "+idade+" anos"
+let texto2 = 'oi "'+nome+'" você tem '+idade+' anos'
+let texto3 = `oi ${nome} você tem ${idade} anos`
+
+console.log(texto1);
+console.log(texto2);
+console.log(texto3);
+
