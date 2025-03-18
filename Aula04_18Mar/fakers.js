462ff2748e713e8c62969e8c443f75eeb64e1d01
mmamorim <marc.m.amorim@gmail.com>
2025-03-18 20:25:26 -0300

    v1

--- Aula04_18Mar/fakers.js
+++ Aula04_18Mar/fakers.js
@@ -1,16 +1,25 @@
 import nomes from "./fakedata/nomes.js"
+import sobrenomes from "./fakedata/sobrenomes.js"
 
 function fakeName() {
     let num = parseInt(Math.random()*nomes.length)
     return nomes[num]
 }
 
-export function fakeUser() {
+function fakeSobrenome() {
+    let num = parseInt(Math.random()*sobrenomes.length)
+    return sobrenomes[num]
+}
 
+export function fakeUser() {
+    let firstName = fakeName()
+    let lastName = fakeSobrenome()
+    let username = firstName.charAt(0)+lastName
+    username = username.toLowerCase()
     return {
-        firstName: fakeName(),
-        lastName: "",
-        username: "",
+        firstName,
+        lastName,
+        username,
         password: "",
         rg: "",
         email: ""    
